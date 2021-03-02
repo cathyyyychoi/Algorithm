37929e103081566ee4126a60bd57d22ba876a631
unknown <chlrkdud777@khu.ac.kr>
2021-03-02 17:02:32 +0900

    prac

---
+++ Baekjoon/15650.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+
+num_list = [i + 1 for i in range(n)]
+check_list = [False] * n
+
+arr = []
+
+def dfs(cnt):
+    if(cnt == m):
+        print(*arr)
+        return
+    
+    for i in range(0, n):
+        if(check_list[i]):
+            continue
+        
+        check_list[i] = True
+        arr.append(num_list[i])
+        dfs(cnt + 1)
+        arr.pop()
+        
+        for j in range(i + 1, n):
+            check_list[j] = False
+            
+        
+dfs(0)
