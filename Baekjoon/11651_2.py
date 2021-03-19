b8b3120d3753513a259ed4e97fa23d9bdf87170c
cathyyyychoi <cathyyyychoi@gmail.com>
2021-03-19 19:44:34 +0900

    prac

---
+++ Baekjoon/11651_2.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+n_list = []
+
+for i in range(n):
+    n_list.append(list(map(int, input().split())))
+n_list.sort(key = lambda x: (x[1], x[0]))
+for i in n_list:
+    print(i[0], i[1])
