513c44edac5db92d9bae7cc0dde76dbfd2b23b3e
cathyyyychoi <cathyyyychoi@gmail.com>
2021-05-11 16:19:57 +0900

    fix errors

--- Baekjoon/python/11653.py
+++ Baekjoon/python/11653.py
@@ -1,7 +1,12 @@
 num = int(input())
+result = []
 
 while num != 1:
     for i in range(2, num + 1):
-        if (num % 2 == 0):
-            print(i)
+        if (num % i == 0):
+            result.append(i)
             num = num // i
+            break
+        
+for i in result:
+    print(i)
