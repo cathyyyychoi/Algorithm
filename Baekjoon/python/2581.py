22febce0f0f3a446650c431d4d7b1cbf0660de72
cathyyyychoi <cathyyyychoi@gmail.com>
2021-05-20 12:16:14 +0900

    prac

---
+++ Baekjoon/python/2581.py
@@ -0,0 +1,20 @@
+M = int(input())
+N = int(input())
+num_list = []
+
+for i in range(M, N + 1):
+    count = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                count += 1
+                break
+        if count == 0:
+            num_list.append(i)
+
+        
+if len(num_list) != 0:
+    print(sum(num_list))
+    print(num_list[0])
+else:
+    print(-1)
