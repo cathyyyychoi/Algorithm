46f0906eb3a8a7ccfb82e8bea2e3d58f16f2a9f0
cathyyyychoi <cathyyyychoi@gmail.com>
2021-05-22 23:09:53 +0900

    basic c++

---
+++ Baekjoon/C++/1008.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  //C++ 더 고민해보기
+    double a;
+    double b;
+
+    scanf("%lf %lf", &a, &b);
+    printf("%.13lf", a / b);
+
+    return 0;
+}
