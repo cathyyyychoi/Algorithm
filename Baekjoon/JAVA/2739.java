49e18c3783b58c4109d804c57fb039d36e113d7f
cathyyyychoi <cathyyyychoi@gmail.com>
2021-07-24 12:59:53 +0900

    compile error

--- Baekjoon/JAVA/2739.java
+++ Baekjoon/JAVA/2739.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args){
-    System.out.println("Hello World!");
+    Scanner sc = new Scanner(System.in);
+
+    int num = sc.nextInt();
+    sc.close();
+
+    for (int i = 1; i <= 9; i++){
+      System.out.println(n + " * " + i + " = " + (num * i));
+    }
   }
 }
