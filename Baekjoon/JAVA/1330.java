f8fd533a21fc1f66277981309ccd59ce6ce198b0
cathyyyychoi <cathyyyychoi@gmail.com>
2021-08-10 00:01:05 +0900

    add annotation

--- Baekjoon/JAVA/1330.java
+++ Baekjoon/JAVA/1330.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
+//기본적으로 import해서 사용
 
 public class Main{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
+    //scanner 객체 생성
+    //System.in을 사용하여 기보드 입력 값을 읽고 원하는 타입으로 변환하여 리턴
 
     final int A = sc.nextInt();
     final int B = sc.nextInt();
+    //nextInt: 다음 토큰을 int 타입으로 리턴
 
     if (A > B)
       System.out.println(">");
