d2597f72a1cbb452c454ccf11cd3b37d73f4d829
cathyyyychoi <cathyyyychoi@gmail.com>
2021-08-26 23:46:00 +0900

    prac for basic java

--- Baekjoon/JAVA/9498.java
+++ Baekjoon/JAVA/9498.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
-    
+
     int score = in.nextInt();
     in.close();
-    
+
     if (score >= 90)
       System.out.println("A");
     else if (score >= 80)
@@ -14,7 +14,7 @@ public class Main{
     else if (score >= 70)
       System.out.println("C");
     else if (score >= 60)
-      System.ouy.println("D");
+      System.out.println("D");
     else
       System.out.println("F");
   }
