bb45a2cb1a0f9cfd2b9fce25ba0142d31596853d
cathyyyychoi <64525308+cathyyyychoi@users.noreply.github.com>
2021-09-04 22:37:55 +0900

    prac for basic java

--- Baekjoon/JAVA/2739.java
+++ Baekjoon/JAVA/2739.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
 
-    int num = sc.nextInt();
-    sc.close();
+    int num = in.nextInt();
+    in.close();
 
     for (int i = 1; i <= 9; i++){
-      System.out.println(n + " * " + i + " = " + (num * i));
+      System.out.println(num + " * " + i + " = " + (num * i));
     }
   }
 }
